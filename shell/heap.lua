d07055302c9772ec6ad7c2035da66f2cc015fdd9
Rene K. Mueller <spiritdude@gmail.com>
2018-01-04 15:49:26 +0100

    fixed 'time', added 'reboot', 'heap'

---
+++ shell/heap.lua
@@ -0,0 +1,11 @@
+-- == Heap ==
+-- Author: Rene K. Mueller <spiritdude@gmail.com>
+-- Description: just print current heap (most important value)
+--
+-- History:
+-- 2018/01/04: 0.0.1: first version
+
+return function(arg)
+   print(node.heap())
+end
+
---
+++ shell/reboot.lua
@@ -0,0 +1,11 @@
+-- == Reboot ==
+-- Author: Rene K. Mueller <spiritdude@gmail.com>
+-- Description: just restart device
+--
+-- History:
+-- 2018/01/04: 0.0.1: first version
+
+return function(arg)
+   print("rebooting now ...")
+   node.restart()
+end
