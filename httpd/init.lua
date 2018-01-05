a6c50759d09c686770cd4d4d0d5eae20cdbd7cca
Rene K. Mueller <spiritdude@gmail.com>
2018-01-05 19:35:01 +0100

    ...

--- httpd/init.lua
+++ httpd/init.lua
@@ -1,9 +1,4 @@
 conf = dofile("httpd/httpd.conf")
 
 print("INFO: httpd starting at port "..conf.port);
-if true then
-   dofile("httpd/simple.lua")
-else 
-   dofile('httpd/httpServer.lua')
-   httpServer:listen(conf.port)
-end
+dofile("httpd/simple.lua")
