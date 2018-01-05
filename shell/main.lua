6ec64be6168ba557fac618e00718fb68e1998817
Rene K. Mueller <spiritdude@gmail.com>
2018-01-05 10:08:11 +0100

    proper string parsing as arguments

--- shell/main.lua
+++ shell/main.lua
@@ -48,10 +48,57 @@ telnet_srv:listen(port, function(socket)
         --node.input(l)           -- works like pcall(loadstring(l)) but support multiple separate line
         l = string.gsub(l,"[\n\r]*$","")
         a = { }
-        string.gsub(l,"([^ ]+)",function(c) 
-          a[#a+1] = c
-          --print("="..c)
-        end)
+        if true then
+          local s = 0               -- state: 0 (default), 1 = non-space, 2 = in " string, 3 = in ' string
+          local t = ""              -- current token
+          local ln = string.len(l)
+          for i=1,ln,1 do
+             local c = string.sub(l,i,i)
+             if(s == 0 and c == '"') then
+               s = 2
+             elseif(s == 0 and c == "'") then
+               s = 3
+             elseif(s == 0 and c == " ") then
+               s = s
+             elseif(s == 0) then
+               t = t..c;
+               s = 1
+             elseif(s == 1) then
+               if(c == " ") then
+                  table.insert(a,t)
+                  t = ""
+                  s = 0
+               else
+                  t = t..c;
+               end
+             elseif(s == 2) then
+               if(c == '"') then
+                  table.insert(a,t)
+                  t = ""
+                  s = 0
+               else
+                  t = t..c;
+               end
+             elseif(s == 3) then
+               if(c == "'") then
+                  table.insert(a,t)
+                  t = ""
+                  s = 0
+               else
+                  t = t..c;
+               end
+             end
+          end
+          if(string.len(t) > 0) then
+            table.insert(a,t)
+          end
+        else
+           -- crude space separating arguments (no strings ".." or '..' parsed)
+           string.gsub(l,"([^ ]+)",function(c) 
+             a[#a+1] = c
+             --print("="..c)
+           end)
+        end
         --for k,v in ipairs(a) do
         --  print(k.."="..v)
         --end
