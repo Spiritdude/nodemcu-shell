b83d0ad858bb1b9064844c16e5737ff026b201bf
Rene K. Mueller <spiritdude@gmail.com>
2018-01-16 20:55:41 +0100

    shell and (future) editor getting along: terminal.* and console.* more consistent

--- shell/clear.lua
+++ shell/clear.lua
@@ -3,10 +3,11 @@
 -- Description: clear screen (VT100)
 --
 -- History:
+-- 2018/01/16: 0.0.2: terminal.* changed, terminal.print() it is
 -- 2018/01/07: 0.0.1: first version
 
 return function(...) 
    --console.print(string.format("%c[%d;%dH%c[J",27,1,1,27))
-   terminal.output(string.format("%c[%d;%dH%c[J",27,1,1,27))
+   terminal.print(string.format("%c[%d;%dH%c[J",27,1,1,27))
 end
 
