b44664e3bc71c4d08f5222bad8440375e6ff0829
Rene K. Mueller <spiritdude@gmail.com>
2018-01-17 21:53:57 +0100

    show at boot if float or integer firmware is installed

--- startup.lua
+++ startup.lua
@@ -3,7 +3,7 @@ node.setcpufreq(node.CPU160MHZ)  -- 2x the speed
 dofile("lib/integer.lua")
 dofile("lib/console.lua")
 dofile("lib/syslog.lua")
-syslog.print(syslog.INFO,"device #"..node.chipid()..string.format(" / 0x%x",node.chipid()).." starting up")
+syslog.print(syslog.INFO,"device #"..node.chipid()..string.format(" / 0x%x",node.chipid()).." starting up ("..(1/2==0 and "integer" or "float").." firmware)")
 --dofile("display/init.lua")
 dofile("wifi/init.lua")
 
